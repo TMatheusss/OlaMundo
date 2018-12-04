6a584549652e4c1cf889fe3267826473c555d279
TMatheusss <thiagomatheus@hotmail.com>
2018-12-04 16:29:00 -0300

    Teste

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Aperte tp thiago on your keyboard to reload,\n' +
+    'Apert tp thiago on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
